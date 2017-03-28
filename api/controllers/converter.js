3343edf0140821fcf4911af955ff653c97011a5f
Ladi Adenusi <ladisays@gmail.com>
2017-03-28 20:27:58 +0100

    updated template to save to image

--- api/controllers/converter.js
+++ api/controllers/converter.js
@@ -2,17 +2,17 @@ var pdf = require("html-pdf");
 var pug = require("pug");
 var path = require("path");
 var options = {
-    height: "720px",
-    width: "960px",
+    orientation: "landscape",
+    type: "jpeg",
+    quality: "100",
     directory: "/public/files"
 };
 
 function toPDF(req, res) {
     var menu = req.body;
-    console.log(menu);
     var template = pug.renderFile("api/templates/menu.pug", menu);
     
-    pdf.create(template, options).toFile("./public/files/menu.pdf", function (err, data) {
+    pdf.create(template, options).toFile("./public/files/menu.jpeg", function (err, data) {
         if (err) {
             console.log(err);
             return res.status(400).json(err);
